dbf98e00ce49f9eb017f4834a5c3e633f82f3227
DevYeon <yeonkevin@icloud.com>
2018-08-08 13:58:59 +0900

    Portfolio Models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Portfolio(models.Model):
+    thumnail_image = models.ImageField()
+    item_title = models.CharField(max_length=20)
+    item_content = models.TextField()
+    github_link = models.URLField(max_length=200)
