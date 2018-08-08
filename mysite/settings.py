771f8883db769a203155b5015310fafd414e355e
DevYeon <yeonkevin@icloud.com>
2018-08-08 14:59:48 +0900

    add static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_URL = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = 'portfolio/media'
 MEDIA_URL = '/media/'
